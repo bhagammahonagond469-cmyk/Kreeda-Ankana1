92f9e481ae86f1bde358acf23bee619ed2958a3f
bhagammahonagond469-cmyk <bhagammahonagond469@gmail.com>
2026-05-18 18:09:08 +0530

    Add files via upload

---
+++ _com_kreedaankana_KreedaAnkanaApp.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.kreedaankana.KreedaAnkanaApp",
+    rootPackage = "com.kreedaankana",
+    originatingRoot = "com.kreedaankana.KreedaAnkanaApp",
+    originatingRootPackage = "com.kreedaankana",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "KreedaAnkanaApp",
+    originatingRootSimpleNames = "KreedaAnkanaApp"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_kreedaankana_KreedaAnkanaApp {
+}
